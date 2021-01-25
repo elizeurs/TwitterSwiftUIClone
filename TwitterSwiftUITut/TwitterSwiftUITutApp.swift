c0f44ad43e7be899a1900b8244f5eb27b17550c6
elizeurs <13ders@gmail.com>
2021-01-25 19:43:51 -0300

    Initial Commit

---
+++ TwitterSwiftUITut/TwitterSwiftUITutApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterSwiftUITutApp.swift
+//  TwitterSwiftUITut
+//
+//  Created by Elizeu RS on 25/01/21.
+//
+
+import SwiftUI
+
+@main
+struct TwitterSwiftUITutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
