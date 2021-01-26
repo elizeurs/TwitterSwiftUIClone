bb169510ce67084483ba983a0713180ddd6f7b9c
elizeurs <13ders@gmail.com>
2021-01-26 10:32:58 -0300

    First commit: creating a tab view

--- TwitterSwiftUITut/ContentView.swift
+++ TwitterSwiftUITut/ContentView.swift
@@ -8,14 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+  var body: some View {
+    NavigationView {
+      
+      TabView {
+        Text("Feed")
+          .tabItem {
+            Image(systemName: "house")
+            Text("Home")
+          }
+        
+        Text("Search View")
+          .tabItem {
+            Image(systemName: "magnifyingglass")
+            Text("Search")
+          }
+        
+        Text("Message")
+          .tabItem {
+            Image(systemName: "envelope")
+            Text("Messages")
+          }
+      }
+      .navigationBarTitle("Home")
+      .navigationBarTitleDisplayMode(.inline)
     }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
