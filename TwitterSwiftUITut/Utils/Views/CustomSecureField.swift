9d9689774546dea1abb6b47d6d8c4f238dcc290a
elizeurs <13ders@gmail.com>
2021-01-28 19:06:16 -0300

    Finishing Up Login Screen

--- TwitterSwiftUITut/Utils/Views/CustomSecureField.swift
+++ TwitterSwiftUITut/Utils/Views/CustomSecureField.swift
@@ -1,5 +1,5 @@
 //
-//  CustomTextField.swift
+//  CustomSecureField.swift
 //  TwitterSwiftUITut
 //
 //  Created by Elizeu RS on 28/01/21.
@@ -7,10 +7,9 @@
 
 import SwiftUI
 
-struct CustomTextField: View {
+struct CustomSecureField: View {
   @Binding var text: String
   let placeholder: Text
-  let imageName: String
   
   var body: some View {
     ZStack(alignment: .leading) {
@@ -21,22 +20,21 @@ struct CustomTextField: View {
       }
       
       HStack(spacing: 16) {
-        Image(systemName: "envelope")
+        Image(systemName: "lock")
           .resizable()
           .scaledToFit()
           .frame(width: 20, height: 20)
           .foregroundColor(.white)
         
-        TextField("", text: $text)
+        SecureField("", text: $text)
         
       }
     }
   }
 }
 
-
-struct CustomTextField_Previews: PreviewProvider {
-  static var previews: some View {
-    CustomTextField(text: .constant(""), placeholder: Text("Email"), imageName: "envelope")
-  }
+struct CustomSecureField_Previews: PreviewProvider {
+    static var previews: some View {
+      CustomSecureField(text: .constant(""), placeholder: Text("Password"))
+    }
 }
