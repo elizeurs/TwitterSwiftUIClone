a282e916f133a7a19300e852ff37a660b07331ce
elizeurs <13ders@gmail.com>
2021-01-28 16:20:36 -0300

    Building Upload Tweet View

--- TwitterSwiftUITut/View/FeedView.swift
+++ TwitterSwiftUITut/View/FeedView.swift
@@ -8,33 +8,38 @@
 import SwiftUI
 
 struct FeedView: View {
-    var body: some View {
-      ZStack(alignment: .bottomTrailing) {
-        ScrollView {
-          VStack {
-            ForEach(0..<20) { _ in
-              TweetCell()
-            }
-          }.padding()
-        }
-        
-        Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
-          Image("tweet")
-            .resizable()
-            .renderingMode(.template)
-            .frame(width: 32, height: 32)
-            .padding()
-        })
-        .background(Color(.systemBlue))
-        .foregroundColor(.white)
-        .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
-        .padding()
+  @State var isShowingNewTweetView = false
+  
+  var body: some View {
+    ZStack(alignment: .bottomTrailing) {
+      ScrollView {
+        VStack {
+          ForEach(0..<20) { _ in
+            TweetCell()
+          }
+        }.padding()
+      }
+      
+      Button(action: { isShowingNewTweetView.toggle() }, label: {
+        Image("tweet")
+          .resizable()
+          .renderingMode(.template)
+          .frame(width: 32, height: 32)
+          .padding()
+      })
+      .background(Color(.systemBlue))
+      .foregroundColor(.white)
+      .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+      .padding()
+      .fullScreenCover(isPresented: $isShowingNewTweetView) {
+        NewTweetView(isPresented: $isShowingNewTweetView)
       }
     }
+  }
 }
 
 struct FeedView_Previews: PreviewProvider {
-    static var previews: some View {
-        FeedView()
-    }
+  static var previews: some View {
+    FeedView()
+  }
 }
