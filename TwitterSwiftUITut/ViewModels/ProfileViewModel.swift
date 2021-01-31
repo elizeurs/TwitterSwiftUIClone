58d12d095b4401ea5a5338c80a42edbca8539212
elizeurs <13ders@gmail.com>
2021-01-31 20:13:40 -0300

    Unfollow User

--- TwitterSwiftUITut/ViewModels/ProfileViewModel.swift
+++ TwitterSwiftUITut/ViewModels/ProfileViewModel.swift
@@ -18,15 +18,25 @@ class ProfileViewModel: ObservableObject {
   
   func follow() {
     guard let  currentUid =  Auth.auth().currentUser?.uid else { return }
+    let followingRef = COLLECTION_FOLLOWING.document(currentUid).collection("user-following")
+    let followerRef = COLLECTION_FOLLOWERS.document(self.user.id).collection("user-followers")
     
-    COLLECTION_FOLLOWING.document(currentUid).collection("user-following").document(user.id).setData([:]) { _ in
-      COLLECTION_FOLLOWERS.document(self.user.id).collection("user-followers").document(currentUid).setData([:]) { _ in
+    followingRef.document(user.id).setData([:]) { _ in
+      followerRef.document(currentUid).setData([:]) { _ in
         self.isFollowed = true
       }
     }
   }
   
   func unfollow() {
+    guard let  currentUid =  Auth.auth().currentUser?.uid else { return }
+    let followingRef = COLLECTION_FOLLOWING.document(currentUid).collection("user-following")
+    let followerRef = COLLECTION_FOLLOWERS.document(self.user.id).collection("user-followers")
     
+    followingRef.document(user.id).delete { _ in
+      followerRef.document(currentUid).delete { _ in
+        self.isFollowed = false
+      }
+    }
   }
 }
