1ca00ecbdd2b5c730111d237156a4fdb7ba7dbbf
elizeurs <13ders@gmail.com>
2021-02-01 11:14:23 -0300

    Check If User Is Followed

--- TwitterSwiftUITut/ViewModels/ProfileViewModel.swift
+++ TwitterSwiftUITut/ViewModels/ProfileViewModel.swift
@@ -14,6 +14,7 @@ class ProfileViewModel: ObservableObject {
   
   init(user: User) {
     self.user = user
+    checkIfUserIsFollowed()
   }
   
   func follow() {
@@ -38,5 +39,16 @@ class ProfileViewModel: ObservableObject {
         self.isFollowed = false
       }
     }
+  }
+  
+  func checkIfUserIsFollowed() {
+    guard let currentUid = Auth.auth().currentUser?.uid else { return }
+    let followingRef = COLLECTION_FOLLOWING.document(currentUid).collection("user-following")
+    
+    followingRef.document(user.id).getDocument { snapshot, _ in
+      guard let isFollowed = snapshot?.exists else { return }
+      self.isFollowed = isFollowed
+    }
+    
   }
 }
